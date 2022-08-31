467e3ef781b29d3996d0e0eab55f10f48764e4ec
Deniz Gökbaş <dng.develops@gmail.com>
2022-08-31 23:52:36 +0300

    Generic repository pattern created in core layer and than implemented in data access layer

--- DataAccess/Abstract/IOperationClaimDal.cs
+++ DataAccess/Abstract/IOperationClaimDal.cs
@@ -8,8 +8,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IUserDal : IEntityRepository <User>
+    public interface IOperationClaimDal: IEntityRepository<OperationClaim>
     {
-
+            
     }
 }
