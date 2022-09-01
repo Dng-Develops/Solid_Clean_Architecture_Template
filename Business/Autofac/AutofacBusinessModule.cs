6e9411276bb350c6e899ab022dd4a567dbf22f6d
Deniz Gökbaş <dng.develops@gmail.com>
2022-09-02 00:47:36 +0300

    User and useroperationClam services completed

--- Business/Autofac/AutofacBusinessModule.cs
+++ Business/Autofac/AutofacBusinessModule.cs
@@ -3,6 +3,7 @@ using Business.Abstract;
 using Business.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntitiyFramework;
+using DataAccess.Concrete.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,8 +16,17 @@ namespace Business.Autofac
     {
         protected override void Load(ContainerBuilder builder)
         {
+            //OperationClaim
             builder.RegisterType<OperationClaimManager>().As<IOperationClaimService>();
             builder.RegisterType<EfOperationClaimDal>().As<IOperationClaimDal>();
+            
+            //User
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>(); 
+            
+            //UserOperationClaim
+            builder.RegisterType<UserOperationClaimManager>().As<IUserOperationClaimService>();
+            builder.RegisterType<EfUserOperationClaimDal>().As<IUserOperationClaimDal>();
         }
         
 
