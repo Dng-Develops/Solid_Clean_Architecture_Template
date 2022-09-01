45810b078ac7382221821028bf7db3f511322cd1
Deniz Gökbaş <dng.develops@gmail.com>
2022-09-01 23:52:11 +0300

    Business layer manager service added and Operation claim added + Autofac dependency injecton IoC container implemented

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,16 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business.Abstract;
+using Business.Autofac;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntitiyFramework;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Autofac configurations
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModule()));
 
 // Add services to the container.
 
@@ -6,6 +18,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
